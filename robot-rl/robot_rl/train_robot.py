d6bdb0dd83a5271a64f94b250fc3dcb494ffd64f
Ashis Ghosh <ashisghosh94@gmail.com>
2024-06-01 20:01:28 -0700

    fix: re-implement visualization for train

--- robot-rl/robot_rl/train_robot.py
+++ robot-rl/robot_rl/train_robot.py
@@ -1,12 +1,11 @@
-import os
 import yaml
 import copy
 import gymnasium as gym
-import gymnasium_robotics  # This registers Gymnasium-Robotics environments
 import argparse
-from stable_baselines3 import PPO, HerReplayBuffer
+from stable_baselines3 import PPO, HerReplayBuffer  # noqa: F401
 from sb3_contrib import TQC
-#from sb3_contrib import TRPO
+
+# from sb3_contrib import TRPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecNormalize
 from sb3_contrib.common.wrappers import TimeFeatureWrapper
@@ -46,7 +45,7 @@ with open("hyperparam/rl-zoo.yml") as f:
         raise ValueError(f"Hyperparameters not found for {args.env_id}")
 
 env_config = hyperparams_dict[args.env_id]
-env_config.update({'env_id': args.env_id})
+env_config.update({"env_id": args.env_id})
 
 callback = None
 if not args.disable_logging:
@@ -58,30 +57,43 @@ if not args.disable_logging:
         save_code=True,  # optional
         name=args.env_id + "_" + str(args.seed) + "_TRPO",
     )
-    callback = WandbCallback(
-        gradient_save_freq=200000,
-        model_save_freq=200000,
-        model_save_path=f"models/{args.env_id}",
-        verbose=2,
-    ),
+    callback = (
+        WandbCallback(
+            gradient_save_freq=200000,
+            model_save_freq=200000,
+            model_save_path=f"models/{args.env_id}",
+            verbose=2,
+        ),
+    )
+
 
 def make_env():
     render_mode = "human" if args.visualize_episodes else None
-    env = gym.make(args.env_id, render_mode = render_mode)
+    env = gym.make(args.env_id, render_mode=render_mode)
 
     env = Monitor(env)  # record stats such as returns
     env = TimeFeatureWrapper(env)
 
     return env
 
-env = DummyVecEnv([make_env])
 
-normalize_kwargs = {"gamma": hyperparams["gamma"]}
+if not args.visualize_episodes:
+    env = DummyVecEnv([make_env])
 
-env = VecNormalize(env, **normalize_kwargs)
-# Get the env_wrapper hyperparam
+    normalize_kwargs = {"gamma": hyperparams["gamma"]}
 
-env = VecVideoRecorder(env, f"videos/{args.env_id}_{args.seed}", record_video_trigger=lambda x: x % 200000 == 0, video_length=200)
+    env = VecNormalize(env, **normalize_kwargs)
+    # Get the env_wrapper hyperparam
+
+    env = VecVideoRecorder(
+        env,
+        f"videos/{args.env_id}_{args.seed}",
+        record_video_trigger=lambda x: x % 200000 == 0,
+        video_length=200,
+    )
+else:
+    env = make_env()
+
 
 hyperparams["policy_kwargs"] = eval(hyperparams["policy_kwargs"])
 hyperparams["replay_buffer_kwargs"] = eval(hyperparams["replay_buffer_kwargs"])
@@ -89,8 +101,16 @@ hyperparams["replay_buffer_kwargs"] = eval(hyperparams["replay_buffer_kwargs"])
 n_timesteps = copy.deepcopy(hyperparams["n_timesteps"])
 del hyperparams["n_timesteps"]
 
-model = TQC(env=env, replay_buffer_class=HerReplayBuffer, verbose=1,  seed=args.seed, device='cuda', tensorboard_log=f"runs/{args.env_id}_{args.seed}_1", **hyperparams) 
-# model = TRPO(env=env, verbose=1,  seed=args.seed, device='cuda', tensorboard_log=f"runs/{args.env_id}_{args.seed}_1", **hyperparams) 
+model = TQC(
+    env=env,
+    replay_buffer_class=HerReplayBuffer,
+    verbose=1,
+    seed=args.seed,
+    device="cuda",
+    tensorboard_log=f"runs/{args.env_id}_{args.seed}_1",
+    **hyperparams,
+)
+# model = TRPO(env=env, verbose=1,  seed=args.seed, device='cuda', tensorboard_log=f"runs/{args.env_id}_{args.seed}_1", **hyperparams)
 
 # tensorboard_log=f"runs/{run.id}",
 
--- robot-rl/robot_rl/visualize_env.py
+++ robot-rl/robot_rl/visualize_env.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 
 # Define your custom environment
 env_id = "FetchPickAndPlace-v2"
-env = gym.make(env_id, render_mode='human')
+env = gym.make(env_id, render_mode="human")
 
 # Reset the environment
 obs = env.reset()
@@ -14,7 +14,7 @@ try:
 
         # Take a random action
         action = env.action_space.sample()
-        
+
         # Uncomment this if you want to use a pre-trained model
         # action, _ = model.predict(obs)
 
